a208bbe0ed5767bb369ee0b9b644bf64150dd4fb
deathsy <kittipong.sri@mail.kmutt.ac.th>
2018-02-21 17:36:01 +0700

    [ADD] get user Favourite service

--- api/src/main/java/com/travesist/api/controllers/FavouriteController.java
+++ api/src/main/java/com/travesist/api/controllers/FavouriteController.java
@@ -3,12 +3,21 @@ package com.travesist.api.controllers;
 import com.travesist.api.services.FavouriteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/favourite")
 public class FavouriteController {
 
     @Autowired
     private FavouriteService favouriteService;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public List getAllFavourites (@RequestParam("userId") Long userId) {
+        return favouriteService.getUserFavService(userId);
+    }
 }
