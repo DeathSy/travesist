d37359d4a16d1fa40f8053082aec6bb8b6e5cca5
penthaizza <veeraporn.m@mail.kmutt.ac.th>
2018-02-21 17:16:48 +0700

    [EDIT] change route to render here

--- site/src/App.js
+++ site/src/App.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Input from './components/input'
-import Maps from './components/map'
+import Here from './components/here'
 
 export default () => (
   <Router>
     <div>
-      <Route exect path='/' component={Maps} />
+      <Route exect path='/' component={Here} />
     </div>
   </Router>
 )
