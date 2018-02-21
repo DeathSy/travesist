b19eb53b9c558d878a3618b86db66877e872c15e
deathsy <kittipong.sri@mail.kmutt.ac.th>
2018-02-22 04:43:29 +0700

    [ADD] suggesting and routing feature

--- site/src/components/input/input.js
+++ site/src/components/input/input.js
@@ -1,10 +1,27 @@
 import React from 'react'
-import {Input} from 'semantic-ui-react'
+import { Input } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 
-const InputField = () => (
-    <div style={{ display: 'flex', width: '100%', padding: 10, zIndex: 100 }}>
-        <Input icon='search' placeholder='Search' style={{ flex: 1 }} />
-    </div>
+const style = {
+  inputWrapper: {
+    display: 'flex',
+    width: '100%',
+    height: 72,
+    padding: 10,
+    zIndex: 100,
+    position: 'absolute',
+    fontSize: 22
+  }
+}
+
+const InputField = ({ onChange }) => (
+  <div style={style.inputWrapper}>
+    <Input
+      icon='search'
+      placeholder='Search'
+      style={{ flex: 1 }}
+      onChange={onChange}
+    />
+  </div>
 )
-export default InputField 
+export default InputField
