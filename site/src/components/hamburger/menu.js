3cff62ce905d9f6a9f6de7286be11fb7625f2cfc
Your Name <ntk-work@hotmail.com>
2018-02-21 22:57:57 +0700

    [EDIT] menu styles color

--- site/src/components/hamburger/menu.js
+++ site/src/components/hamburger/menu.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import { Dropdown, Icon, Menu } from 'semantic-ui-react'
 
-const Menu = () => (
-    <div>
-        <Menu attached='top'>
+
+const MenuBar = () => (
+    
+    <div style={{display: 'flex', background:'#f2711c', zIndex: 100, borderTopLeftRadius: '4.5px', 
+    borderBottomLeftRadius: '4.5px'}}>
+        <Menu color='orange' inverted>
             <Dropdown item icon='content' simple>
                 <Dropdown.Menu>
                     <Dropdown.Item>Profile</Dropdown.Item>
-                    <Dropdown.Item>Favorite</Dropdown.Item>
                     <Dropdown.Item>History</Dropdown.Item>
+                    <Dropdown.Item>Favorite</Dropdown.Item>
                 </Dropdown.Menu>
-            </Dropdown>     
+            </Dropdown>
         </Menu>
-
     </div>
 )
 
-export default Menu
+export default MenuBar
